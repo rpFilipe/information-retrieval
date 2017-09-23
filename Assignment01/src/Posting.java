25ac40d33c09b0a0d958b054f1bb6f5f681f304e
Ricardo Filipe <ricardo.filipe_@hotmail.com>
2017-09-23 14:34:17 +0100

    Netbeans project

---
+++ Assignment01/src/Posting.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rpfilipe
+ */
+public class Posting implements Comparable{
+    
+    
+    private int docId;
+    private int frequency;
+
+    public Posting(int docId) {
+        this.docId = docId;
+        this.frequency = 1;
+    }
+
+    public int getDocId() {
+        return docId;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+
+    public int compareTo(Object o) {
+
+        Posting p = (Posting) o;
+
+        if(this.docId < p.docId)
+            return -1;
+
+        if(this.docId > p.docId)
+            return 1;
+
+        return 0;
+    }
+}

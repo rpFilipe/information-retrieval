b20b218e16ab68f8559d75bab566a9b1ba7cfc0c
Joana Conde <joanaconde@gmail.com>
2017-10-09 16:51:47 +0100

    Choose tokenizer added to the main.

--- Assignment01/src/Assignment01.java
+++ Assignment01/src/Assignment01.java
@@ -9,6 +9,7 @@ import Structures.Posting;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.xml.parsers.ParserConfigurationException;
@@ -31,12 +32,12 @@ public class Assignment01 {
         try {
             Document doc;
             String cLocation = args[0];
-
+            Scanner in = new Scanner(System.in);
+            
             System.out.println("Reading the corpus from " + cLocation);
             System.out.println("Loading stopwords list from " + args[1]);
             CorpusReader cr;
-            //Tokenizer stk = new SimpleTokenizer(Integer.parseInt(args[3]));
-            Tokenizer ctk = new ComplexTokenizer(args[1], args[2], Integer.parseInt(args[3]));
+            Tokenizer ctk = null, stk = null; 
             // estrutura de dados com os tokens
             List<String> tokenList = new ArrayList<>();
             Indexer indx = new Indexer();
@@ -44,7 +45,21 @@ public class Assignment01 {
 
             // para testar um numero limitado de corpus
             int count = 0;
-
+            System.out.println("Choose the type of Tokenizer that you want to use: ");
+            String input = "";
+            while(!input.equalsIgnoreCase("complex") || !input.equalsIgnoreCase("simple") ){
+                System.out.println("Write simple or complex...");
+                input = in.nextLine();
+                if(input.equalsIgnoreCase("complex")){
+                    ctk = new ComplexTokenizer(args[1], args[2], Integer.parseInt(args[3]));
+                    break;
+                }
+                if(input.equalsIgnoreCase("simple")){
+                    stk = new SimpleTokenizer(Integer.parseInt(args[3]));
+                    break;
+                }
+            }
+            
             System.out.println("Document Processor initialized...");
             try {
                 cr = new CorpusReader(cLocation);
@@ -52,15 +67,21 @@ public class Assignment01 {
                 //cr.printCorpusDocuments();
                 while (cr.hasNext()) {
                     doc = ((Document) cr.next());
-                    //tokenList = ctk.contentProcessor(doc.getContent());
-                    tokenList = ctk.contentProcessor(doc.getContent());
+                    
+                    if(input.equalsIgnoreCase("complex"))
+                        tokenList = ctk.contentProcessor(doc.getContent());
+                    else
+                        tokenList = stk.contentProcessor(doc.getContent());
+
                     indx.indexDoc(doc.getDocId(), tokenList);
                      
                 }
                 indx.saveToFile(args[4]);
-                //indx.getTermInOneDoc();
-                //ndx.getTermHigherFreq();
                 System.out.println("Document Processor finished...");
+                System.out.println("\nResults:\nList the ten first terms (in alphabetic order) that appear in only one document:");
+                System.out.println(indx.getTermInOneDoc());
+                System.out.println("\nList the ten terms with higher document frequency:");
+                System.out.println(indx.getTermHigherFreq());
             } catch (SAXException ex) {
                 Logger.getLogger(Assignment01.class.getName()).log(Level.SEVERE, null, ex);
             }

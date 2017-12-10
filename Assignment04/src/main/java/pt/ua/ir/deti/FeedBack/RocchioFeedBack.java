814b5efb72d28307a7eb6ad936faf6fd4b383ec7
Joana Conde <joanaconde@gmail.com>
2017-12-10 16:48:25 +0000

    Docs relevant and non relevante fo explicit feedback.

--- Assignment04/src/main/java/pt/ua/ir/deti/FeedBack/RocchioFeedBack.java
+++ Assignment04/src/main/java/pt/ua/ir/deti/FeedBack/RocchioFeedBack.java
@@ -1,4 +1,3 @@
-
 package pt.ua.ir.deti.FeedBack;
 
 import java.io.File;
@@ -9,13 +8,16 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
+import static java.util.stream.Collectors.toMap;
 import pt.ua.deti.ir.Structures.QueryResult;
 import pt.ua.deti.ir.Structures.Relevance;
 import pt.ua.deti.ir.Structures.StringPosting;
 
 /**
- * Universidade de Aveiro, DETI, Recuperação de Informação 
- * @author Joana Conde 
+ * Universidade de Aveiro, DETI, Recuperação de Informação
+ *
+ * @author Joana Conde
  * @author Ricardo Filipe
  */
 public class RocchioFeedBack {
@@ -58,59 +60,65 @@ public class RocchioFeedBack {
 
     /**
      * Metodo para carregar o gold standart relevance
+     *
      * @param fname
-     * @throws FileNotFoundException 
+     * @throws FileNotFoundException
      */
     private void loadRelevance(String fname) throws FileNotFoundException {
         File fileidx = new File(fname);
         Scanner fsc = new Scanner(fileidx);
         this.relevanceMap = new HashMap<>();
-        
+
         String[] line;
         int currentQueryId = 1;
         int queryId, docId, relevance;
         Relevance rl;
         LinkedList<Relevance> docs = new LinkedList();
-        
-        while(fsc.hasNext()) {
+
+        while (fsc.hasNext()) {
             line = fsc.nextLine().split(" ");
-            
+
             queryId = Integer.parseInt(line[0]);
             docId = Integer.parseInt(line[1]);
             relevance = Integer.parseInt(line[2]);
-            
+
             rl = new Relevance(queryId, docId, relevance);
 
-            if (currentQueryId != queryId){
+            if (currentQueryId != queryId) {
                 relevanceMap.put(currentQueryId, docs);
                 docs = new LinkedList();
                 docs.add(rl);
                 currentQueryId = queryId;
-            }
-            else{
+            } else {
                 docs.add(rl);
             }
-        } 
-        
+        }
+
         relevanceMap.put(currentQueryId, docs);
     }
 
-    public Map<String, Double> computeFeedBack(String type, int queryId, Map<String, Double> queryVector, TreeSet<QueryResult> retireveDocs) {
+    public Map<String, Double> computeFeedBack(String type, int queryId, Map<String, Double> queryVector, TreeSet<QueryResult> retriveDocs) {
 
         LinkedList relevantDocs = relevanceMap.get(queryId);
-        if(relevantDocs == null)
+        if (relevantDocs == null) {
             return queryVector;
-      
-        
+        }
+
         //int dr = relevantDocs.size();
         //int dnr = corpusSize - dr;
         
-        
-        
-        if (type.equalsIgnoreCase("explicit")) {
-            
-            
 
+        if (type.equalsIgnoreCase("explicit")) {
+
+            TreeSet<QueryResult> dr = retriveDocs.stream()
+                    .filter(e -> relevantDocs.contains(e.getDocId()))
+                    .collect(Collectors.toCollection(TreeSet<QueryResult>::new));
+
+            TreeSet<QueryResult> dnr = retriveDocs.stream()
+                    .filter(e -> !relevantDocs.contains(e.getDocId()))
+                    .collect(Collectors.toCollection(TreeSet<QueryResult>::new));
+
+            
         } // type = implicit
         else {
 
